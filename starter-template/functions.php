c477cba80735b68608d35f38eaba985a8f467da3
avivnog <53513607+avivnog@users.noreply.github.com>
2021-09-30 11:02:21 +0300

    Update functions.php

--- starter-template/functions.php
+++ starter-template/functions.php
@@ -286,28 +286,7 @@ function mycustom_flamingo_map_meta_cap($caps, $cap, $user_id, $args)
         $caps[] = $meta_caps[$cap];
     return $caps;
 }
-add_action('init', function () {
-    register_post_type('testimonials', array(
-        'labels' => array(
-            'name' => __('Testimonials'),
-            'singular_name' => __('Testimonial')
-        ),
-        'public' => true,
-        'has_archive' => true,
-        'rewrite' => array('slug' => 'testimonials'),
-        'supports' => array('thumbnail', 'editor', 'title')
-    ));
-    register_post_type('news', array(
-        'labels' => array(
-            'name' => __('News'),
-            'singular_name' => __('News')
-        ),
-        'public' => true,
-        'has_archive' => true,
-        'rewrite' => array('slug' => 'news'),
-        'supports' => array('thumbnail', 'editor', 'title')
-    ));
-});
+
 function upload_svg_files($allowed)
 {
     if (!current_user_can('manage_options'))
